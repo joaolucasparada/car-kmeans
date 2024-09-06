62309f2751083ba64ebc38c49a92cdfbf22031ab
João Lucas Parada <143855439+joaolucasparada@users.noreply.github.com>
2024-09-05 21:56:40 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,10 +24,12 @@ col2.write('Matriz de correlação: ')
 fig = px.imshow(data1.corr().round(2), color_continuous_scale='plasma', text_auto=True)
 col2.plotly_chart(fig)
 
+col3, col4 = st.columns(2)
+col1.metric(label='Variance Ratio', value='59%')
+col2.metric(label='Variance', value='4.75')
+
 st.write('K-Means clusters:')
 fig1 = px.strip(projection, x='x',y='y',color='cluster_pca', color_discrete_sequence=['blue', 'yellow', 'orange', 'green', 'purple'],  hover_data=['x', 'y', 'Car', 'Year'])
 st.plotly_chart(fig1)
 
-col1, col2 = st.columns([2,6])
-col1.metric(label='Variance Ratio', value='59%')
-col2.metric(label='Variance', value='4.75')
+
